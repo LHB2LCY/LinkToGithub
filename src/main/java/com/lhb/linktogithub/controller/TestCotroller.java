4966b49dd7fbbb0b0e917e90bd56335ab4f7c4c2
liangshuang <297089375@qq.com>
2021-07-10 10:15:56 +0800

    添加第一行代码注释

--- src/main/java/com/lhb/linktogithub/controller/TestCotroller.java
+++ src/main/java/com/lhb/linktogithub/controller/TestCotroller.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestCotroller {
 
+
+    /*
+    * 第一行代码
+    * */
     @GetMapping("/hello")
     public String sayHello(){
         return "hello LiHaoBiao";
